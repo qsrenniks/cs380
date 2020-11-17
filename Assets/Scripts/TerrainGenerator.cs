e4d9f9f50d03e6f06ac03882beae7d0339fff0f7
qsrenniks <q.skinner@digipen.edu>
2020-11-17 12:02:48 -0800

    added several tile types to the terrain generator

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -5,60 +5,69 @@ using UnityEngine.Tilemaps;
 
 public class TerrainGenerator : MonoBehaviour
 {
-    public Tilemap grid;
-    public Tile test; 
-    public int pixWidth = 10;
-    public int pixHeight = 10;
+  public Tilemap grid;
+  public Tile water;
+  public Tile land;
+  public Tile rock;
+  public int pixWidth = 10;
+  public int pixHeight = 10;
 
-    public float xOrg; 
-    public float yOrg; 
+  public float xOrg;
+  public float yOrg;
 
-    public float scale = 1.0f;
+  public float scale = 1.0f;
 
-    private Color[] pix;
+  private Color[] pix;
 
-    void Start()
+  void Start()
+  {
+    pix = new Color[pixWidth * pixHeight];
+  }
+
+  void CalcNoise()
+  {
+    // For each pixel in the texture...
+    float y = 0.0F;
+
+    while (y < pixWidth)
     {
-        pix = new Color[pixWidth * pixHeight];
+      float x = 0.0F;
+      while (x < pixHeight)
+      {
+        float xCoord = xOrg + x / pixWidth * scale;
+        float yCoord = yOrg + y / pixHeight * scale;
+        float sample = Mathf.PerlinNoise(xCoord, yCoord);
+        pix[(int)y * pixWidth + (int)x] = new Color(sample, sample, sample);
+        x++;
+      }
+      y++;
     }
 
-    void CalcNoise()
-    {
-        // For each pixel in the texture...
-        float y = 0.0F;
+    // Copy the pixel data to the texture and load it into the GPU.
+    //noiseTex.SetPixels(pix);
+    //noiseTex.Apply();
 
-        while (y < pixWidth)
+    for (int i = 0; i < pixWidth; ++i)
+    {
+      for (int j = 0; j < pixHeight; ++j)
+      {
+        Vector3 start = new Vector3(0.0f, 0.0f, 0.0f);
+        start.x += i;
+        start.y += j;
+        if (pix[i * pixWidth + j].r < 0.5f)
         {
-            float x = 0.0F;
-            while (x < pixHeight)
-            {
-                float xCoord = xOrg + x / pixWidth * scale;
-                float yCoord = yOrg + y / pixHeight * scale;
-                float sample = Mathf.PerlinNoise(xCoord, yCoord);
-                pix[(int)y * pixWidth + (int)x] = new Color(sample, sample, sample);
-                x++;
-            }
-            y++;
+          grid.SetTile(grid.WorldToCell(start), water);
         }
-
-        // Copy the pixel data to the texture and load it into the GPU.
-        //noiseTex.SetPixels(pix);
-        //noiseTex.Apply();
-
-        for(int i = 0; i < pixWidth; ++i)
-        {
-            for(int j = 0; j < pixHeight; ++j)
-            {
-                Vector3 start = new Vector3(0.0f, 0.0f, 0.0f);
-                start.x += i;
-                start.y += j;
-                grid.SetTile(grid.WorldToCell(start), test);
-            }
+        else
+        { 
+          grid.SetTile(grid.WorldToCell(start), land);
         }
+      }
     }
+  }
 
-    void Update()
-    {
-        CalcNoise();
-    }
+  void Update()
+  {
+    CalcNoise();
+  }
 }
